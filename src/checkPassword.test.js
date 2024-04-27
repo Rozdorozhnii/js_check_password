4bb92e13093c210a59ffa5e4a422fd3f6e9ffa2b
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-04-27 20:33:51 +0300

    Solution_v1.0

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,35 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = checkPassword('');
 
+    expect(typeof result).toBe('boolean');
+  });
+
+  it(`should return 'false' for password with non-Latin character`, () => {
+    expect(checkPassword('Pяssword1!')).toBe(false);
+  });
+
+  it(`should return 'false' for password without any uppercase char`, () => {
+    expect(checkPassword('password1!')).toBe(false);
+  });
+
+  it(`should return 'false' for password without any digit`, () => {
+    expect(checkPassword('Password!')).toBe(false);
+  });
+
+  it(`should return 'false' for password without any special char`, () => {
+    expect(checkPassword('Password1')).toBe(false);
+  });
+
+  it(`should return 'false' for too weak password`, () => {
+    expect(checkPassword('Passw1!')).toBe(false);
+  });
+
+  it(`should return 'false' for too long password`, () => {
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('Passwo1!')).toBe(true);
   });
-
-  // write more tests here
 });
